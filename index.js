65efad111c768c3fbf18741bb93e0e82d7224630
djChika <etka12865%>
2018-07-21 14:00:26 +0300

    add news loader from 2ch.hk/news

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Telegraf = require('telegraf');
 const settings = require('./settings');
+const axios = require('axios');
 
 const bot = new Telegraf(settings.token, settings.options);
 
@@ -15,6 +16,28 @@ bot.command('title', ctx => {
 
 bot.command('chatid', ctx => {
     ctx.getChat().then(chat => console.log(chat.id));
+});
+
+
+bot.command('news', ctx => {
+    url = 'https://2ch.hk/news/catalog.json';
+    axios.get(url).then(response => {
+        base_url = 'https://2ch.hk';
+        post_base_url = 'https://2ch.hk/news/res/';
+
+        let news = response.data.threads[Math.floor(Math.random() * 5) + 2];
+
+        let subject = news['subject'];
+        let comment = news['comment'];
+        let picture = base_url + news['files'][0]['path'];
+        let post_url = post_base_url + news['num'] + '.html';
+
+        ctx.reply(subject);
+        ctx.replyWithPhoto(picture);
+        ctx.reply('Подробнее: ' + post_url)
+    }).catch(error => {
+        console.log(error);
+    });
 });
 
 bot.startPolling();
